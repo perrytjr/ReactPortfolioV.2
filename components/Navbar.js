4a81e141fb9b875f61091b0619771c921e85a70c
Perry Theobald <perrytjr@gmail.com>
2021-03-17 17:46:37 -0700

    navbar css bootstrap

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,19 +1,19 @@
 import styles from '../styles/Navbar.module.css'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Navbar, Nav,  } from 'react-bootstrap'
 
 const Navigation = () => {
   return (
     <>
-      <div>
-        <Navbar  fixed="top" bg="light" expand="lg">
-          <Navbar.Brand href="/">Perry Theobald Jr</Navbar.Brand>
+      <div >
+        <Navbar  fixed="top" bg="dark" expand="lg" variant="dark">
+          <Navbar.Brand  href="/">Perry Theobald Jr</Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <Nav.Link href="/about">About</Nav.Link>
-              <Nav.Link href="/projects">Projects</Nav.Link>
-              <Nav.Link href="/contact">Contacts</Nav.Link>
-              <Nav.Link href="/skills">Skills</Nav.Link>
+            <Nav  className="ml-auto" >
+              <Nav.Link id="link" className="m-3" id="link" href="/about">About</Nav.Link>
+              <Nav.Link id="link"className="m-3" href="/projects">Projects</Nav.Link>
+              <Nav.Link id="link" className="m-3" href="/contact">Contacts</Nav.Link>
+              <Nav.Link id="link" className="m-3" href="/skills">Skills</Nav.Link>
             </Nav>
 
           </Navbar.Collapse>
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from 'next/head'
-import Navbar from '../components/Navbar'
-import Sidebar from '../components/Sidebar'
+
 import styles from '../styles/Layout.module.css'
 
 export default function Home() {
@@ -11,12 +10,8 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-    <Navbar />
-    <div class='row'>
-      <div class='col-sm'>
-    <Sidebar/>
-      </div>
-    </div>
+   
+  
    
 
     </div>
